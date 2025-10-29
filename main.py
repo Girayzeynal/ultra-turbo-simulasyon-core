f991f6bf20dea2073f0fca649ebfe25c1f76826c
Girayzeynal <girayzeynal@gmail.com>
2025-10-29 20:12:38 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,3 +3,16 @@ def baslat():
 
 if __name__ == "__main__":
     baslat()
+from telegram.ext import Updater, CommandHandler
+
+def start(update, context):
+    update.message.reply_text("MK Ultra Turbo Simülasyon Core Aktif ✅")
+
+updater = Updater(token=os.environ["BOT_TOKEN"], use_context=True)
+dispatcher = updater.dispatcher
+
+dispatcher.add_handler(CommandHandler("start", start))
+
+print("MK Ultra Turbo Simulasyon Core Baslatildi ✅")
+updater.start_polling()
+updater.idle()
